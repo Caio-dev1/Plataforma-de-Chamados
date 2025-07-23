0d017ff5382c2974d3728dff717dfac2adeca577
Caio-dev1 <caiocesar.developer1@gmail.com>
2025-07-23 19:41:37 -0300

    .

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,11 @@
+import * as S from "./StyledElements";
+
+function Login() {
+    return ( 
+        <S.LoginSection>
+            <S.LoginForm></S.LoginForm>
+        </S.LoginSection>
+    );
+}
+
+export default Login;
---
+++ src/Pages/Login/StyledElements.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const LoginSection = styled.section`
+ background-color: #F9FAFA;
+`
+
+export const LoginForm = styled.form`
+
+`
